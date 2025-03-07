2894963c5b1f906fa4894577ef47ccecea96d0a1
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-03-07 16:32:02 -0500

    added some test code for trying out the handmademath library; added pthread compile and linker option

--- src/vulkan-sandbox/main.c
+++ src/vulkan-sandbox/main.c
@@ -24,6 +24,8 @@ int main (void) {
         HMM_Vec4 vec4 = HMM_V4(0, 10, 20, 30);
         HMM_Mat4 mat4 = HMM_M4();
         mat4.Elements[3][0] = -1.f;
+        HMM_Vec4 result = HMM_MulM4V4(mat4, vec4);
+        fprintf(stdout, "first element of resulting vector is: %f\n", result.Elements[0]);
     }
 
     while (!glfwWindowShouldClose(window)) {
