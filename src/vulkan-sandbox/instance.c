992f117f465c916840949c2e70cfeea0c8ec05cc
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-03-13 13:35:01 -0400

    expanded listing of requested extensions and layers with compile definitions

--- src/vulkan-sandbox/instance.c
+++ src/vulkan-sandbox/instance.c
@@ -9,10 +9,75 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if (defined DEBUG) && (defined _WIN32)
+#warning "Untested platform, list of requested layers might be inaccurate"
+static const char * requestedExtensions[] = {
+    "VK_KHR_win32_surface",
+    VK_KHR_SURFACE_EXTENSION_NAME,
+    VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
+};
+static const uint32_t nRequestedExtensions = sizeof(requestedExtensions) / sizeof(requestedExtensions[0]);
+static const char * requestedLayers[] = {
+    "VK_LAYER_KHRONOS_validation",
+};
+static const uint32_t nRequestedLayers = sizeof(requestedLayers) / sizeof(requestedLayers[0]);
+#elif (defined DEBUG) && (defined __APPLE__)
+#warning "Untested platform, list of requested layers might be inaccurate"
+static const char * requestedExtensions[] = {
+    "VK_MVK_macos_surface",
+    VK_KHR_SURFACE_EXTENSION_NAME,
+    VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
+};
+static const uint32_t nRequestedExtensions = sizeof(requestedExtensions) / sizeof(requestedExtensions[0]);
+static const char * requestedLayers[] = {
+    "VK_LAYER_KHRONOS_validation",
+};
+static const uint32_t nRequestedLayers = sizeof(requestedLayers) / sizeof(requestedLayers[0]);
+#elif (defined DEBUG)
+#if (!defined __linux__)
+#warning "Unsupported platform, assuming Linux"
+#endif
+static const char * requestedExtensions[] = {
+    "VK_KHR_xcb_surface",  // not sure why the enum equivalent of this is not recognized
+    VK_KHR_SURFACE_EXTENSION_NAME,
+    VK_EXT_DEBUG_UTILS_EXTENSION_NAME,
+};
+static const uint32_t nRequestedExtensions = sizeof(requestedExtensions) / sizeof(requestedExtensions[0]);
+static const char * requestedLayers[] = {
+    "VK_LAYER_KHRONOS_validation",
+};
+static const uint32_t nRequestedLayers = sizeof(requestedLayers) / sizeof(requestedLayers[0]);
+#elif (!defined DEBUG) && (defined _WIN32)
+#warning "Untested platform, list of requested layers might be inaccurate"
+static const char * requestedExtensions[] = {
+    "VK_KHR_win32_surface",
+};
+static const uint32_t nRequestedExtensions = sizeof(requestedExtensions) / sizeof(requestedExtensions[0]);
+static const char ** requestedLayers = nullptr;
+static const uint32_t nRequestedLayers = 0;
+#elif (!defined DEBUG) && (defined __APPLE__)
+#warning "Untested platform, list of requested layers might be inaccurate"
+static const char * requestedExtensions[] = {
+    "VK_MVK_macos_surface",
+};
+static const uint32_t nRequestedExtensions = sizeof(requestedExtensions) / sizeof(requestedExtensions[0]);
+static const char ** requestedLayers = nullptr;
+static const uint32_t nRequestedLayers = 0;
+#elif (!defined DEBUG)
+#if (!defined __linux__)
+#warning "Unsupported platform, assuming Linux"
+#endif
+static const char * requestedExtensions[] = {
+    "VK_KHR_xcb_surface",
+    "VK_KHR_surface",
+};
+static const uint32_t nRequestedExtensions = sizeof(requestedExtensions) / sizeof(requestedExtensions[0]);
+static const char ** requestedLayers = nullptr;
+static const uint32_t nRequestedLayers = 0;
+#endif
 
-
-void print_instance_extension_properties (const VkInstanceCreateInfo * createInfo);
-void print_instance_layer_properties (const VkInstanceCreateInfo * createInfo);
+static void print_instance_extension_properties (const VkInstanceCreateInfo * createInfo);
+static void print_instance_layer_properties (const VkInstanceCreateInfo * createInfo);
 
 
 void instance_destroy (VkInstance * instance) {
@@ -22,18 +87,11 @@ void instance_destroy (VkInstance * instance) {
 
 
 VkInstance instance_init (void) {
-    uint32_t enabledExtensionCount;
-    const char ** ppEnabledExtensionNames = glfwGetRequiredInstanceExtensions(&enabledExtensionCount);
 
-#ifdef DEBUG
-    uint32_t enabledLayerCount = 1;
-    const char * ppEnabledLayerNames[] = {
-        "VK_LAYER_KHRONOS_validation"
-    };
-#else
-    uint32_t enabledLayerCount = 0;
-    const char ** ppEnabledLayerNames = nullptr;
-#endif
+    if (glfwVulkanSupported() != GLFW_TRUE) {
+        fprintf(stderr, "According to GLWF, Vulkan is not supported here, aborting.\n");
+        exit(EXIT_FAILURE);
+    }
 
     VkApplicationInfo applicationInfo = {
         .apiVersion = VK_API_VERSION_1_0,
@@ -46,12 +104,12 @@ VkInstance instance_init (void) {
     };
 
     VkInstanceCreateInfo createInfo = {
-        .enabledExtensionCount = enabledExtensionCount,
-        .enabledLayerCount = enabledLayerCount,
+        .enabledExtensionCount = nRequestedExtensions,
+        .enabledLayerCount = nRequestedLayers,
         .pApplicationInfo = &applicationInfo,
         .pNext = nullptr,
-        .ppEnabledExtensionNames = ppEnabledExtensionNames,
-        .ppEnabledLayerNames = ppEnabledLayerNames,
+        .ppEnabledExtensionNames = requestedExtensions,
+        .ppEnabledLayerNames = requestedLayers,
         .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
     };
 
@@ -69,7 +127,7 @@ VkInstance instance_init (void) {
 }
 
 
-void print_instance_extension_properties (const VkInstanceCreateInfo * createInfo) {
+static void print_instance_extension_properties (const VkInstanceCreateInfo * createInfo) {
     uint32_t nextensions;
     vkEnumerateInstanceExtensionProperties(nullptr, &nextensions, nullptr);
     fprintf(stdout, "InstanceExtensionProperties (%" PRIu32 "):\n", nextensions);
@@ -89,7 +147,7 @@ void print_instance_extension_properties (const VkInstanceCreateInfo * createInf
 }
 
 
-void print_instance_layer_properties (const VkInstanceCreateInfo * createInfo) {
+static void print_instance_layer_properties (const VkInstanceCreateInfo * createInfo) {
     uint32_t nlayers = 0;
     vkEnumerateInstanceLayerProperties(&nlayers, nullptr);
     fprintf(stdout, "InstanceLayerProperties (%" PRIu32 "):\n", nlayers);
