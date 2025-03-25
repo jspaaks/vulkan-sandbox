44fc6c247806335f3139e14d776cf632f44cda92
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-03-25 09:05:18 -0400

    moved initialization and destruction to dedicated static functions in main.c

--- src/vulkan-sandbox/main.c
+++ src/vulkan-sandbox/main.c
@@ -1,3 +1,4 @@
+#include "images.h"
 #include "instance.h"
 #include "logical-device.h"
 #include "messenger.h"
@@ -13,9 +14,35 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-
-void loop (State * state);
 void cleanup (State * state);
+void init (State * state);
+void loop (State * state);
+
+void cleanup (State * state) {
+    images_destroy(state);
+    swapchain_destroy(state);
+    queue_destroy(state);
+    logical_device_destroy(state);
+    queue_family_destroy(state);
+    physical_device_destroy(state);
+    surface_destroy(state);
+    messenger_destroy(state);
+    instance_destroy(state);
+    window_destroy(state);
+}
+
+void init (State * state) {
+    window_init(state);
+    instance_init(state);
+    messenger_init(state);
+    surface_init(state);
+    physical_device_init(state);
+    queue_family_init(state);
+    logical_device_init(state);
+    queue_init(state);
+    swapchain_init(state);
+    images_init(state);
+}
 
 void loop (State * state) {
     while (!glfwWindowShouldClose(state->window)) {
@@ -23,47 +50,13 @@ void loop (State * state) {
     }
 }
 
-
 int main (void) {
 
-    // --------------   initialize resources   ----------------- //
-
     State state = {};
-    window_init(&state);
-    instance_init(&state);
-    messenger_init(&state);
-    surface_init(&state);
-    physical_device_init(&state);
-    queue_family_init(&state);
-    logical_device_init(&state);
-    queue_init(&state);
-    swapchain_init(&state);
 
-    // ---------------------   main loop   --------------------- //
-
+    init(&state);
     loop(&state);
-
-    // ----------------   clean up resources   ----------------- //
-
-    swapchain_destroy(&state);
-    queue_destroy(&state);
-    logical_device_destroy(&state);
-    queue_family_destroy(&state);
-    physical_device_destroy(&state);
-    surface_destroy(&state);
-    messenger_destroy(&state);
-    instance_destroy(&state);
-    window_destroy(&state);
+    cleanup(&state);
 
     return EXIT_SUCCESS;
 }
-
-//#include "HandmadeMath.h"
-//{
-//    // linear algebra test
-//    HMM_Vec4 vec4 = HMM_V4(0, 10, 20, 30);
-//    HMM_Mat4 mat4 = HMM_M4();
-//    mat4.Elements[3][0] = -1.f;
-//    HMM_Vec4 result = HMM_MulM4V4(mat4, vec4);
-//    fprintf(stdout, "first element of resulting vector is: %f\n", result.Elements[0]);
-//}
