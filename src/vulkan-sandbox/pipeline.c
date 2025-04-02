3137a6252ffad5fd8cca58f155387a4993ed008c
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2025-04-02 12:17:31 -0400

    added skeleton for various pipeline components; added initializing dynamic state; updated initializing shader stages

--- src/vulkan-sandbox/pipeline.c
+++ src/vulkan-sandbox/pipeline.c
@@ -7,18 +7,66 @@
 #include <stdio.h>
 
 static void destroy (State * state);
+static VkPipelineColorBlendStateCreateInfo * get_color_blend_state (void);
+static VkPipelineDepthStencilStateCreateInfo * get_depth_stencil_state (void);
+static VkPipelineDynamicStateCreateInfo * get_dynamic_state (void);
+static VkPipelineInputAssemblyStateCreateInfo * get_input_assembly_state (void);
+static VkPipelineMultisampleStateCreateInfo * get_multisample_state (void);
+static VkPipelineRasterizationStateCreateInfo * get_rasterization_state (void);
+static VkPipelineShaderStageCreateInfo * get_stages (State * state);
+static VkPipelineTessellationStateCreateInfo * get_tessellation_state (void);
+static VkPipelineVertexInputStateCreateInfo * get_vertex_input_state (void);
+static VkPipelineViewportStateCreateInfo * get_viewport_state (void);
 static void init (State * state);
 static VkShaderModule load_shader (State * state, const char * filename);
 
-static void destroy (State *) {
-    // TODO destroy pipeline
+static void destroy (State * state) {
+    const VkAllocationCallbacks * allocator = nullptr;
+    vkDestroyPipeline(state->logical_device, state->pipeline, allocator);
 }
 
-static void init (State * state) {
+static VkPipelineColorBlendStateCreateInfo * get_color_blend_state (void) {
+    // TODO
+    return nullptr;
+}
+
+static VkPipelineDepthStencilStateCreateInfo * get_depth_stencil_state (void) {
+    // TODO
+    return nullptr;
+}
+
+static VkPipelineDynamicStateCreateInfo * get_dynamic_state (void) {
+    constexpr size_t nstates = 2;
+    return & (VkPipelineDynamicStateCreateInfo) {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO,
+        .dynamicStateCount = nstates,
+        .pDynamicStates = (VkDynamicState[nstates]){
+            VK_DYNAMIC_STATE_VIEWPORT,
+            VK_DYNAMIC_STATE_SCISSOR,
+        },
+    };
+}
+
+static VkPipelineInputAssemblyStateCreateInfo * get_input_assembly_state (void) {
+    // TODO
+    return nullptr;
+}
+
+static VkPipelineMultisampleStateCreateInfo * get_multisample_state (void) {
+    // TODO
+    return nullptr;
+}
+
+static VkPipelineRasterizationStateCreateInfo * get_rasterization_state (void) {
+    // TODO
+    return nullptr;
+}
+
+static VkPipelineShaderStageCreateInfo * get_stages (State * state) {
     fprintf(stdout, "TODO fix hardcoded shader paths\n");
-    VkPipelineShaderStageCreateInfo stages[SHADER_STAGE_COUNT] = {};
+    static VkPipelineShaderStageCreateInfo stages[SHADER_STAGE_COUNT] = {};
     {
-        // create vert shader
+        // create vertex shader
         VkShaderModule shader = load_shader(state, "dist/assets/shaders/shader.vert.spv");
         stages[SHADER_STAGE_VERT] = (VkPipelineShaderStageCreateInfo) {
             .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
@@ -26,12 +74,11 @@ static void init (State * state) {
             .module = shader,
             .pName = "main",
         };
-        // TODO pipeline
         // according to the docs, it's ok to destroy the shaders once the pipeline has been created
         vkDestroyShaderModule(state->logical_device, shader, nullptr);
     }
     {
-        // create frag shader
+        // create fragment shader
         VkShaderModule shader = load_shader(state, "dist/assets/shaders/shader.frag.spv");
         stages[SHADER_STAGE_FRAG] = (VkPipelineShaderStageCreateInfo) {
             .sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
@@ -39,9 +86,53 @@ static void init (State * state) {
             .module = shader,
             .pName = "main",
         };
-        // TODO pipeline
         // according to the docs, it's ok to destroy the shaders once the pipeline has been created
         vkDestroyShaderModule(state->logical_device, shader, nullptr);
+    }
+    return &stages[0];
+}
+
+static VkPipelineTessellationStateCreateInfo * get_tessellation_state (void) {
+    // TODO
+    return nullptr;
+}
+
+static VkPipelineVertexInputStateCreateInfo * get_vertex_input_state (void) {
+    // TODO
+    return nullptr;
+}
+
+static VkPipelineViewportStateCreateInfo * get_viewport_state (void) {
+    // TODO
+    return nullptr;
+}
+
+static void init (State * state) {
+    VkGraphicsPipelineCreateInfo info = (VkGraphicsPipelineCreateInfo){
+        .sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .stageCount = SHADER_STAGE_COUNT,
+        .pStages = get_stages(state),
+        .pVertexInputState = get_vertex_input_state(),
+        .pInputAssemblyState = get_input_assembly_state(),
+        .pTessellationState = get_tessellation_state(),
+        .pViewportState = get_viewport_state(),
+        .pRasterizationState = get_rasterization_state(),
+        .pMultisampleState = get_multisample_state(),
+        .pDepthStencilState = get_depth_stencil_state(),
+        .pColorBlendState = get_color_blend_state(),
+        .pDynamicState = get_dynamic_state(),
+    };
+    VkResult result = vkCreateGraphicsPipelines(state->logical_device,
+                                                VK_NULL_HANDLE,
+                                                1,
+                                                &info,
+                                                nullptr,
+                                                &state->pipeline);
+    if (result != VK_SUCCESS) {
+        fprintf(stderr, "Encountered an error while trying to create the graphics pipeline, aborting");
+        exit(EXIT_FAILURE);
     }
 }
 
